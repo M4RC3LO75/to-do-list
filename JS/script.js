fdcd06dbd7590a28108333deda7b267c21b3b031
Marcelo Bastos <marcelobastos1975@gmail.com>
2023-04-27 08:17:10 -0300

    Style Finished

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+// SELEÇÃO DE ELEMENTOS
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#edit-input');
+const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+
+// FUNÇÕES
+
+
+// EVENTOS
+
+todoForm.addEventListener('submit', (e) =>{
+
+    e.preventDefault()
+
+    console.log('Enviou form..')
+
+})
